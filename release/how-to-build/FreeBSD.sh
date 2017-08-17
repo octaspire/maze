eaee3f892fae3c1d1aef5fd04d890a623444c40b
octaspire <octaspire@octaspire.com>
2017-08-18 01:12:52 +0300

    Add 'how-to-build' script for FreeBSD

---
+++ release/how-to-build/FreeBSD.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env sh
+
+YELLOW="$(tput setaf 3 ; tput bold)"
+GREEN="$(tput setaf 2 ; tput bold)"
+NOCOLOR="$(tput setaf 9 ; tput sgr0)"
+
+echoToDefs() { printf "$NOCOLOR\n" ; }
+
+printf "$YELLOW\n"
+cat << EnDoFmEsSaGe
+1. Building the game
+   PLEASE NOTE: This game requires development version of 'SDL2' library
+   (i.e. headers) to be installed on the system; otherwise compilation
+   will fail. Compilation uses also 'pkgconf'. To install development
+   version of library 'SDL2' and 'devel/pkgconf':
+
+       - FreeBSD: sudo pkg install devel/sdl20 devel/pkgconf
+-------------------------------------------------------------------------------
+EnDoFmEsSaGe
+echoToDefs
+read -r BUILDSTR <<'EOF'
+clang -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_MAZE_AMALGAMATED_IMPLEMENTATION octaspire-maze-amalgamated.c -o octaspire-maze `pkgconf --cflags --libs sdl2` -lm
+EOF
+echo $BUILDSTR
+eval $BUILDSTR
+
+
+
+printf "\nDone.\n$GREEN"
+echo   "=================================================================="
+echo   "Run the game like this:"
+echo   "=================================================================="
+printf "%b1)%b ./octaspire-maze\n" $YELLOW $GREEN
+echo "=================================================================="
+echoToDefs
+

a18c8530247bf3b103c140e8ebffe299764fcdc6
octaspire <octaspire@octaspire.com>
2017-08-23 00:29:39 +0300

    Add 'how-to-build'-script for MidnightBSD

---
+++ etc/how-to-build/MidnightBSD.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env sh
+
+YELLOW='\033[1;33m'
+GREEN='\033[1;32m'
+NOCOLOR='\033[0m'
+
+echoToDefs() { printf "$NOCOLOR\n" ; }
+
+printf "$YELLOW\n"
+cat << EnDoFmEsSaGe
+1. Building the game
+   PLEASE NOTE: This game requires development version of 'SDL2' library
+   (i.e. headers) to be installed on the system; otherwise compilation
+   will fail. To install development version of library 'SDL2':
+
+       - MidnightBSD: sudo mport install sdl2
+-------------------------------------------------------------------------------
+EnDoFmEsSaGe
+echoToDefs
+read -r BUILDSTR <<'EOF'
+gcc -O2 -std=c99 -Wall -Wextra -DOCTASPIRE_MAZE_AMALGAMATED_IMPLEMENTATION octaspire-maze-amalgamated.c -o octaspire-maze `sdl2-config --cflags --libs` -lm
+EOF
+echo $BUILDSTR
+eval $BUILDSTR
+
+
+
+printf "\nDone.\n$GREEN"
+echo   "=================================================================="
+echo   "Run the game like this:"
+echo   "=================================================================="
+printf "%b1)%b ./octaspire-maze\n" $YELLOW $GREEN
+echo "=================================================================="
+echoToDefs
+
